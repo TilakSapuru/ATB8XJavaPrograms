0d7b7e7e4d367f1c8f36f17f05892f214501ee4f
TilakSapuru <tilaks1426@gmail.com>
2024-09-10 19:39:07 +0530

    Add The files

---
+++ src/Sept/ex_09092024/Lab005.java
@@ -0,0 +1,26 @@
+package Sept.ex_09092024;
+
+public class Lab005 {
+
+    public static void main(String[] args){
+        // Keywords and Identifiers
+        // Reserved Words
+        // Simpler English - Pre Defined Meaning in java Programs.
+        // They are in blue in colour in intellij
+        // package , public , class , void
+        // They will be in lower case
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ src/Sept/ex_09092024/Lab006.java
@@ -0,0 +1,12 @@
+package Sept.ex_09092024;
+
+public class Lab006  {
+    public static void main(String[] args){
+        // Variables and Data Types, Identifiers
+        boolean Is_Tilak_Married = true;
+        boolean do_you_have_audi =false;
+
+
+
+    }
+}
