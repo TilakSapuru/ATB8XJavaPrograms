67c4ec122bf1f6b2eeae6518ba4cfba7254b206a
TilakSapuru <tilaks1426@gmail.com>
2024-09-10 18:41:38 +0530

    Add The files

--- src/Sept/ex_06092024/Lab004.java
+++ src/Sept/ex_06092024/Lab004.java
@@ -1,6 +1,6 @@
 package Sept.ex_06092024;
 
-public class Lab007 {
+public class Lab004 {
     public static void main(String[] args) {
         // This Code Will not executed
         // This is used to show the user or dev instructions
